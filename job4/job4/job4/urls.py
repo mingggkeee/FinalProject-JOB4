65496b7285dcfc61faa3bb229d1e7a355be2ca8b
ssac-academy <ssac-academy@example.com>
2021-03-31 18:03:12 +0900

    base.html upload

--- job4/job4/job4/urls.py
+++ job4/job4/job4/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import HomeView
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name="Home"),
 ]
