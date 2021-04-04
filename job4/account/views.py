1e1992ba6dbceceb4c42be304209cb091a99c34f
nahyeon-an <skgus923@gmail.com>
2021-04-04 11:44:39 +0900

    update recover id page

--- job4/account/views.py
+++ job4/account/views.py
@@ -10,18 +10,16 @@ from django.conf import settings
 from .models import User
 
 # Create your views here.
+
+
 class LoginView(TemplateView):
     template_name = 'account/login.html'
 
+
 class LoginRequestView(TemplateView):
     def post(self, request):
-        print(request.POST["user-id"])
-        print(User.objects.filter(id=request.POST["user-id"]).exists())
         if request.method == "POST":
-            # user 부분 수정 필요
-            # models.py 에서 수정하고 user 를 가져와서 확인
-            try:
-                if User.objects.filter(id=request.POST["user-id"]).exists():
+            if User.objects.filter(id=request.POST["user-id"]).exists():
                     user = User.objects.get(id=request.POST["user-id"])
 
                     if user.password == request.POST["password"]:
@@ -29,7 +27,7 @@ class LoginRequestView(TemplateView):
                         request.session['is_active'] = True
 
                         remember = request.POST.get('auto', False)
-                        if remember=='on':
+                        if remember == 'on':
                             request.session['remember'] = True
                             settings.SESSION_EXPIRE_AT_BROWSER_CLOSE = False
                         else:
@@ -37,14 +35,16 @@ class LoginRequestView(TemplateView):
                             settings.SESSION_EXPIRE_AT_BROWSER_CLOSE = True
                             request.session.set_expiry(1800)
 
-                        return render(request, 'home.html')
-            except:
-                return render(request, 'account/login.html')
+                        return redirect('/')
+
+            return redirect('/account/login')
+
 
 class LogoutView(View):
     def get(self, request):
         request.session.clear()
         return redirect('/')
+
 
 class RegisterView(TemplateView):
     template_name = 'account/register.html'
@@ -52,58 +52,57 @@ class RegisterView(TemplateView):
     def post(self, request):
         if request.method == "POST":
             if request.POST["password"] == request.POST["repeat-password"]:
-                user = User.objects.create(id=request.POST["id"], \
-                                           password=request.POST["password"], \
-                                           birth=request.POST["birth"], \
-                                           email=request.POST["email"], \
-                                           phone_number=request.POST["phone_number"], \
-                                           address=request.POST["address"], \
+                user = User.objects.create(id=request.POST["id"],
+                                           password=request.POST["password"],
+                                           birth=request.POST["birth"],
+                                           email=request.POST["email"],
+                                           phone_number=request.POST["phone_number"],
+                                           address=request.POST["address"],
                                            gender=request.POST["gender"])
                 return render(request, 'account/register_done.html')
 
-            return render(request, 'account/register.html')
-
-# class RegisterRequestView(TemplateView):
-#     def post(self, request):
-#         if request.method == "POST":
-#             if request.POST["password"] == request.POST["repeat-password"]:
-#                 user = User.objects.create(id=request.POST["id"], \
-#                                            password=request.POST["password"], \
-#                                            birth=request.POST["birth"], \
-#                                            email=request.POST["email"], \
-#                                            phone_number=request.POST["phone_number"], \
-#                                            address=request.POST["address"], \
-#                                            gender=request.POST["gender"])
-#                 return render(request, 'account/register_done.html')
-
-#             return render(request, 'account/register.html')
+            # return render(request, 'account/register.html')
+            return redirect('/account/register')
 
 class RecoverIDView(TemplateView):
     template_name = 'account/recover_id.html'
 
+
 class RecoverIDRequestView(View):
-    # print('recover id')
     def post(self, request):
         if request.method == "POST":
-            # print(request.POST['email'])
-            
             email = request.POST.get('email')
-            result_id = User.objects.get(email=email)
-            print(result_id)
-            return render(request, 'account/register_done.html')
+            if User.objects.filter(email=email).exists() and \
+                User.objects.filter(email=email).count() == 1:
 
-        return render(request, 'account/recover_id_done.html')
+                userObj = User.objects.get(email=email)
+
+                from django.core import mail
+
+                connection = mail.get_connection()
+
+                connection.open()
+
+                email = mail.EmailMessage(
+                    'ID recovery from JOB4',
+                    'This is test email.',
+                    'ssac.job4@gmail.com',
+                    [userObj.email],
+                    connection=connection,
+                )
+                email.send()
+
+                return render(request, 'account/recover_id_done.html')
+
+            else:
+                return render(request, '/account/recover_id')
 
 class RecoverPWView(TemplateView):
     template_name = 'account/recover_pw.html'
 
 class RecoverPWRequestView(View):
-    # print('recover pw')
     def post(self, request):
         if request.method == "POST":
-            # print(request.POST['id'])
-            # print(request.POST['email'])
-
             user_id = request.POST['id']
             result_id = User.objects.get(id=user_id)
             print(result_id.birth)
